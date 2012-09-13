54d77be3d2e9e9c6e0debcf646e560c2aa6f3d2e
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2012-09-14 00:14:49 +0200

    Added dummy implementation for the existing files

---
+++ code/World.hs
@@ -0,0 +1,23 @@
+module World where
+import Data.Array
+
+data Direction = North | South | West | East
+  deriving (Show,Eq,Read,Enum)
+
+type Position = (Int, Int)
+type Cell = [Direction]
+type Maze = Array Int Cell
+move :: Direction -> Position -> Position
+move North  (x, y) = (x, y+1)
+move West   (x, y) = (x-1, y)
+move South  (x, y) = (x, y-1)
+move East   (x, y) = (x+1, y)
+
+moves :: [Direction] -> Position -> Position 
+moves = flip $ foldr move 
+
+validMove :: Maze -> Position -> Position -> Bool
+validMove m p1 p2 = undefined
+
+fromList :: [(Position, [Direction])] -> Maze
+fromList xs = undefined
