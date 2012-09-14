8d4d000297929c44658c16038856a0a86606396d
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2012-09-15 00:32:46 +0200

    Implemented some of the functions

--- code/MEL.hs
+++ code/MEL.hs
@@ -20,15 +20,45 @@ data Stm = Forward
 
 type Program = Stm
 
--- Definition of type is only to make it typecheck
-type World = (String, Maze)
+--newtype State a = S (State a -> (a, State a))
 
---newtype RobotCommand a = RC {runRC :: World -> ... }
+--instance Monad State where
+--  return x = undefined
+--  S f >>= g = undefined
+
+data Robot = Robot  { position :: Position
+                    , direction :: Direction
+                    , history::[Position]
+                    }
+
+type World = (Robot, Maze)
+
+newtype RobotCommand a = RC {runRC :: World -> Robot -> World }
 
 initialWorld :: Maze -> World
-initialWorld m = undefined
+initialWorld m = (Robot{position = (0,0), direction = North, history=[]}, m)
 
--- interp :: Stm -> RobotCommand()
+fromRelative :: Relative -> Direction -> Direction
+
+fromRelative Ahead d    = d
+fromRelative ToLeft d   = pred d
+fromRelative ToRight d  = succ d
+fromRelative Behind d   = succ $ succ d
+
+evalC :: Cond -> Bool
+evalC (Wall r)  = True
+evalC (And c1 c2) = evalC c1 && evalC c2 
+evalC (Not c)   = not $ evalC c
+
+interp :: Stm -> RobotCommand()
+interp Forward      = undefined
+interp Backward     = undefined
+interp TurnRight    = undefined
+interp TurnLeft     = undefined
+interp (If c s0 s1) = if evalC c then interp s0 else interp s1
+interp (While c s)  = undefined
+interp (Block [])   = undefined
+interp (Block [s]) = undefined
 
 runProg :: Maze -> Program -> ([Position], Direction)
 runProg m p = undefined
--- code/World.hs
+++ code/World.hs
@@ -1,14 +1,31 @@
-module World where
-import Data.Array
+module World (Direction(..), Position, Cell, Maze, fromList, move, moves, validMove, toDirection, withinBounds,) where
+import Data.Array as A
 
-data Direction = North | South | West | East
+data Direction = North | East | South | West
   deriving (Show,Eq,Read,Enum)
 
 type Position = (Int, Int)
-type Cell = [Direction]
-type Maze = Array (Int, Int) Cell
+--newtype Position a = Position{getPosition :: (a,a)}
+--instance Monad Position where
+--  return x = Position (x,x)
+--  Position (x,y) >>= f = 
 
+type Cell = [Direction]
+type Maze = Array Position Cell
+
+-- Produces an empty maze with the specified size, starts at (0,0)
+--emptyMaze :: Position -> Maze 
+--emptyMaze p = A.array ((0,0), p) []
+
+-- Generate a new instance of maze with bounds as specified.
+-- Check for walls are lazy
+-- assert bound > (0,0)
+--generateMaze :: Position -> [(Position, Cell)] -> Maze
+--generateMaze =A.array . (,) (0,0)
+ 
+-- Returns a position if the move is valid, else it returns nothing
 move :: Direction -> Position -> Position
+--move d (x,y) | validMove 
 move North  (x, y) = (x, y+1)
 move West   (x, y) = (x-1, y)
 move South  (x, y) = (x, y-1)
@@ -17,8 +34,46 @@ move East   (x, y) = (x+1, y)
 moves :: [Direction] -> Position -> Position 
 moves = flip $ foldr move 
 
-validMove :: Maze -> Position -> Position -> Bool
-validMove m p1 p2 = undefined
+-- Calculates the distance on the board between two positions
+-- largest size doesnt matter, however if they have different signs the maze is
+-- invalid
+dist :: Position -> Position -> Int
+dist (x1,y1) (x2,y2) = x + y 
+  where x = abs $ x1 - x2
+        y = abs $ y1 - y2 
 
-fromList :: [(Position, [Direction])] -> Maze
-fromList xs = undefined
+-- Assert withinBounds p1 & withinBounds p2 
+-- Assert Pos > (0,0)
+-- Assert dist p1 p2 == 1
+toDirection :: Position -> Position -> Maybe Direction
+toDirection (x1,y1) (x2,y2)  | x == 0 && y > 0 = Just North
+                              | x == 0 && y < 0 = Just South
+                              | x > 0 && y == 0 = Just West
+                              | x < 0 && y == 0 = Just East
+                              | otherwise = Nothing
+                              where x = max x1 x2 - min x1 x2
+                                    y = max y1 y2 - min y1 y2
+
+-- Test wether the position is within bounds of the maze
+-- Asserts Pos > (0,0)
+withinBounds :: Maze -> Position -> Bool
+withinBounds m = (>) (snd (A.bounds m))
+
+-- Tests wether a move from one position to the other is valid on a given board
+-- Assert withinBounds p1 & withinBounds p2
+validMove :: Maze -> Position -> Position -> Bool
+validMove m p1 p2 | dist p1 p2 == 1 = undefined
+                    -- Check if the direction has a wall
+                    -- if not then take the cell in that direction and see if it
+                    -- has a wall facing towards us
+                  | otherwise = False 
+
+--listBounds :: [(Position, Cell)]  -> (Position,Position) -> (Position,Position)
+--listBounds [] p = p
+--listBounds (((x1,y1),_):xs) ((x2, y2),(x3,y3)) = listBounds xs ((min x1 x2, min y1 y2), (max x1 x3, max y1 y3))
+
+-- Produces a maze from the cells
+-- Assert Forall (p,c) in xs: withinBounds p
+fromList :: [(Position, Cell)] -> Maze
+--fromList xs = A.array . (listBounds xs ((0,0),(9,9))) xs
+fromList = A.array ((0,0),(9,9))
